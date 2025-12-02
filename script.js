c16418ef668f8adda1020a3eba83fef6d5d85b58
Waadosterr <waadwaad5harith@gmail.com>
2025-12-02 11:58:27 +0300

    fix

--- script.js
+++ script.js
@@ -29,3 +29,6 @@ function checkGuess() {
     document.getElementById("message").textContent = "Too low!";
   }
 }
+import { neon } from '@netlify/neon';
+const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
